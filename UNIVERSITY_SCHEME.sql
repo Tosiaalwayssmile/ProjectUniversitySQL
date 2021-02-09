e4249916c7790e614dca1b8c4058dd7583a87f97
Antonina Matuszek <37584652+Tosiaalwayssmile@users.noreply.github.com>
2021-02-09 20:24:13 +0100

    Update UNIVERSITY_SCHEME.sql

--- UNIVERSITY_SCHEME.sql
+++ UNIVERSITY_SCHEME.sql
@@ -1,29 +1,29 @@
 /****************	Different way of checking if database exists and creating it	****************/
 /*
 IF NOT EXISTS(
-	SELECT * FROM master.dbo.sysdatabases 
-	WHERE NAME = 'UNIVERSITY'
-	)
+    SELECT * FROM master.dbo.sysdatabases 
+    WHERE NAME = 'UNIVERSITY'
+    )
 CREATE DATABASE UNIVERSITY
 GO
 */
 IF EXISTS(
-	SELECT 1 FROM master.dbo.sysdatabases 
-	WHERE NAME = 'UNIVERSITY'
-	) 
+    SELECT 1 FROM master.dbo.sysdatabases 
+    WHERE NAME = 'UNIVERSITY'
+    ) 
 DROP DATABASE UNIVERSITY
 GO
 
 CREATE DATABASE UNIVERSITY
 GO
 
-CREATE TABLE UNIVERSITY..Teachers( 
-	TeacherId		INT	IDENTITY(100,10) 
-	CONSTRAINT PK_TeacherId PRIMARY KEY (TeacherId),
+CREATE TABLE UNIVERSITY..Teachers(
+    TeacherId		INT	IDENTITY(100,10) 
+    CONSTRAINT PK_TeacherId PRIMARY KEY (TeacherId),
     FirstName		VARCHAR(20),
     LastName		VARCHAR(25) NOT NULL,
     Email          VARCHAR(25)
-	CONSTRAINT     NN_TeacherEmail  NOT NULL,
+    CONSTRAINT     NN_TeacherEmail  NOT NULL,
     PhoneNumber   VARCHAR(20),
     HireDate      DATE,
     Salary         MONEY
@@ -33,20 +33,20 @@ CREATE TABLE UNIVERSITY..Teachers(
 GO
 
 CREATE TABLE UNIVERSITY..Students(
-	StudentId    INT
-	CONSTRAINT PK_StudentId PRIMARY KEY (StudentId),
+    StudentId    INT
+    CONSTRAINT PK_StudentId PRIMARY KEY (StudentId),
     FirstName     VARCHAR(20),
     LastName      VARCHAR(25) NOT NULL,
     PhoneNumber   VARCHAR(20),
     BirthDate      DATE NOT NULL,
-	GroupId    INT
+    GroupId    INT
 );
 GO
 
 CREATE TABLE UNIVERSITY..Subjects(
 	SubjectId    INT IDENTITY(1,1) 
 	CONSTRAINT PK_SubjectId PRIMARY KEY (SubjectId), 
-    SubjectName  VARCHAR(30) NOT NULL,
+	SubjectName  VARCHAR(30) NOT NULL,
 	MainTeacherId	INT	
 	CONSTRAINT FK_Subjects_MainTeacherId FOREIGN KEY (MainTeacherId) REFERENCES Teachers (TeacherId)
 );
@@ -54,7 +54,7 @@ GO
 
 CREATE TABLE UNIVERSITY..Buildings(
 	BuildingId   INT IDENTITY(1,1),
-    BuildingName   VARCHAR(20) NOT NULL
+	BuildingName   VARCHAR(20) NOT NULL
 	CONSTRAINT PK_BuildingId PRIMARY KEY (BuildingId)
 );
 GO
@@ -62,15 +62,15 @@ GO
 CREATE TABLE UNIVERSITY..Classrooms(
 	ClassroomId    INT IDENTITY(1,1),
 	CONSTRAINT PK_ClassroomId PRIMARY KEY (ClassroomId),
-    ClassroomName  VARCHAR(20) NOT NULL,
+	ClassroomName  VARCHAR(20) NOT NULL,
 	BuildingId   INT NOT NULL
 	CONSTRAINT FK_Classrooms_BuildingId FOREIGN KEY (BuildingId) REFERENCES Buildings(BuildingId)
 );
 GO
 
 CREATE TABLE UNIVERSITY..Timetables(
-    SubjectId    INT NOT NULL,
-    StudentId  INT NOT NULL,
+        SubjectId    INT NOT NULL,
+        StudentId  INT NOT NULL,
 	TeacherId  INT NOT NULL,
 	ClassroomId    INT NOT NULL,
 	CONSTRAINT FK_Timetables_StudentId FOREIGN KEY (StudentId) REFERENCES Students(StudentId),
@@ -122,7 +122,7 @@ CREATE TABLE UNIVERSITY..Grades (
 	CONSTRAINT PK_GradeId PRIMARY key (GradeId),
 	GradeValue  FLOAT NOT NULL,
 	GradeDate DATE,
-    SubjectId  INT NOT NULL,
+	SubjectId  INT NOT NULL,
 	StudentId  INT NOT NULL,
 	Note VARCHAR(30) NOT NULL,
 	CONSTRAINT FK_Grades_SubjectId FOREIGN KEY (SubjectId) REFERENCES Subjects(SubjectId),
@@ -143,7 +143,7 @@ GO
 CREATE TABLE UNIVERSITY..FinalGrades (
 	AlbumId INT NOT NULL,
 	FinalGrade  INT,
-    SubjectId  INT NOT NULL,
+	SubjectId  INT NOT NULL,
 	AcademicYearId INT NOT NULL,
 	CONSTRAINT FK_FinalGrades_SubjectId FOREIGN KEY (SubjectId) REFERENCES Subjects(SubjectId),
 	CONSTRAINT FK_FinalGrades_AlbumId FOREIGN KEY (AlbumId) REFERENCES Albums(AlbumId),
