961f7f914b4e5f02b2351e89bd254458b12d83d0
Antonina Matuszek <tosiaalwayssmile@gmail.com>
2021-02-11 13:56:50 +0100

    Update first trigger

--- UNIVERSITY_TRIGGERS.sql
+++ UNIVERSITY_TRIGGERS.sql
@@ -1,80 +1,82 @@
 Use UNIVERSITY;
------------------------TRIGGER 1-----------------------------------
---Wyzwalacz GRADE_ADD_CHECK uniemo¿liwia wprowadzenie oceny cz¹stkowej z przedmiotu, na który dany student nie chodzi.
-IF OBJECT_ID ('GRADE_ADD_CHECK' , 'TR') IS NOT NULL  
-   DROP trigger GRADE_ADD_CHECK ;
-go
-create trigger GRADE_ADD_CHECK 
- on grades instead of insert
-as
-begin
-	declare @student_id int = (select student_id from inserted);
-	declare @subject_id int = (select subject_id from inserted);
-	declare @sub_id int = (SELECT  t.subject_id FROM students s, timetables t
-						   WHERE s.student_id = t.student_id AND @student_id = t.student_id AND @subject_id = t.subject_id)
-	declare @note VARCHAR(30) = (select note from inserted),
-			@grade int = (select grade_name from inserted),
-		    @date datetime = (select grade_date from inserted),
-			@name VARCHAR(45) = (select first_name + ' ' + last_name from students where student_id = @student_id),
-			@subject_name VARCHAR(30) = (select subject_name from subjects where subject_id = @subject_id)
+/***************************************** TRIGGER #1 ********************************************/
+-- EN: Trigger CHECK_INSERTED_GRADE does not allow inserting a grade when student is not attending 
+-- selected course (subject)
+IF OBJECT_ID ('CHECK_INSERTED_GRADE' , 'TR') IS NOT NULL  
+    DROP TRIGGER CHECK_INSERTED_GRADE ;
+GO
+CREATE TRIGGER CHECK_INSERTED_GRADE 
+ON Grades INSTEAD OF INSERT
+AS
+BEGIN
+	DECLARE @StudentId INT = (SELECT StudentId FROM INSERTED);
+	DECLARE @InsertedSubjectId INT = (SELECT SubjectId FROM INSERTED);
+	DECLARE @AttendendSubjectId INT = (SELECT t.SubjectId FROM Students s INNER JOIN Timetables t
+    ON s.StudentId = t.StudentId
+    WHERE @StudentId = t.StudentId AND @InsertedSubjectId = t.SubjectId)
 
-	if(@subject_id = @sub_id)
-		begin
-		INSERT INTO UNIVERSITY..grades VALUES (@grade,@date,@subject_id,@student_id,@note)
-		print 'Zapisano ' + 'Student ' + @name + ' na przedmiot: ' + @subject_name + '.';
-		end
-		else
-		print 'Student ' + @name + ' nie jest zapisany na przedmiot: ' + @subject_name + '.'
-end
+	DECLARE @Note VARCHAR(30) = (SELECT Note FROM INSERTED),
+			@GradeValue INT = (SELECT GradeValue FROM INSERTED),
+		    @GradeDate DATE = (SELECT GradeDate FROM INSERTED),
+			@StudentName VARCHAR(55) = (SELECT FirstName + ' ' + LastName FROM Students WHERE StudentId = @StudentId),
+			@SubjectName VARCHAR(30) = (SELECT SubjectName FROM Subjects WHERE SubjectId = @InsertedSubjectId)
 
-go
+	IF(@InsertedSubjectId = @AttendendSubjectId)
+    BEGIN
+		INSERT INTO UNIVERSITY..Grades VALUES (@GradeValue, @GradeDate, @InsertedSubjectId, @StudentId, @Note)
+		PRINT 'Student ' + @StudentName + ' got a ' + CAST(@GradeValue AS VARCHAR) + ' in ' + @SubjectName + '.';
+		END
+		ELSE
+		PRINT 'Student ' + @StudentName + ' does not attend course: ' + @SubjectName + '.'
+END
+GO
 
- INSERT INTO UNIVERSITY..grades VALUES (2,'2018/02/01',1,640,'Test'); -- nie jest zapisany 
- INSERT INTO UNIVERSITY..grades VALUES (5,'2018/02/01',1,660,'Test'); -- nie jest zapisany
- INSERT INTO UNIVERSITY..grades VALUES (5,'2018/02/01',3,640,'Project');  -- jest zapisany
- --select* from grades
- go
+ INSERT INTO UNIVERSITY..Grades VALUES (2, '2018/02/01', 1, 640, 'Test'); -- should not assign grade
+ INSERT INTO UNIVERSITY..Grades VALUES (5, '2018/02/01', 1, 660, 'Test'); -- should not assign grade
+ INSERT INTO UNIVERSITY..Grades VALUES (5, '2021/02/11', 3, 640, 'Project');  -- should  assign grade
+ --SELECT * FROM Grades WHERE StudentId = 640 AND GradeDate = '2021/02/11' AND GradeValue = 5 AND SubjectId = 3
+ GO
 
  -----------------------TRIGGER 2-----------------------------------
 -- Wyzwalacz TEACHER_HIRE_DATE, który po wstawieniu nauczyciela bez daty zatrudnienia, wstawia aktualn¹.
 --IF OBJECT_ID (TEACHER_HIRE_DATE , 'TR') IS NOT NULL  
--- DROP trigger TEACHER_HIRE_DATE ;
-go
-create trigger TEACHER_HIRE_DATE
+-- DROP TRIGGER TEACHER_HIRE_DATE ;
+GO
+create TRIGGER TEACHER_HIRE_DATE
 on teachers
 after insert
 as
-declare @teacher_id int
-set @teacher_id = (select teacher_id from inserted)
-if (select teachers.hire_date from teachers where teachers.teacher_id =
+DECLARE @teacher_id INT
+set @teacher_id = (SELECT teacher_id FROM INSERTED)
+IF (SELECT teachers.hire_date FROM teachers WHERE teachers.teacher_id =
 @teacher_id) is null
-begin
+BEGIN
 update teachers
 set hire_date = GETDATE() 
-where teachers.teacher_id = @teacher_id
-end
-go
-select * from teachers
+WHERE teachers.teacher_id = @teacher_id
+END
+GO
+SELECT * FROM teachers
 INSERT INTO UNIVERSITY..teachers VALUES ( 2010, 'Niemam', 'Dgaty', '','515.124.4567', NULL, 2670);
-select * from teachers where teachers.teacher_id = 2010
+SELECT * FROM teachers WHERE teachers.teacher_id = 2010
 
 -----------------------TRIGGER 3-----------------------------------
 
 
 -----------------------TRIGGER 3-----------------------------------
---Wyzwalacz ENTER_FINAL_GRADES, który po dodaniu do tabeli academic_year nowego roku akademickiego 
+--Wyzwalacz ENTER_FINAL_GRADES, który po dodaniu do tabeli academic_year noweGO roku akademickieGO 
 --wprowadza 'final grade' jako zaokr¹glon¹ œredni¹ ocen cz¹stkowych.
 
  IF OBJECT_ID ('ENTER_FINAL_GRADES' , 'TR') IS NOT NULL
- DROP trigger ENTER_FINAL_GRADES ;
-go
+ DROP TRIGGER ENTER_FINAL_GRADES ;
+GO
 CREATE TRIGGER ENTER_FINAL_GRADES
 ON academic_year
 INSTEAD OF INSERT
 AS
 BEGIN
 	--start kursor_studenci
-	DECLARE  @student_id int
+	DECLARE  @student_id INT
 	DECLARE kursor_studenci CURSOR FOR
 	SELECT student_id FROM students;
 	OPEN kursor_studenci
@@ -83,7 +85,7 @@ BEGIN
 		WHILE @@FETCH_STATUS = 0
 		BEGIN
 			--start kursor_przedmioty
-			DECLARE @subject_id int 
+			DECLARE @subject_id INT 
 			DECLARE kursor_przedmioty CURSOR FOR
 			SELECT subject_id FROM subjects;
 			OPEN kursor_przedmioty
@@ -93,9 +95,9 @@ BEGIN
 				BEGIN
 					IF EXISTS (SELECT student_id FROM grades WHERE @student_id = student_id AND @subject_id = subject_id)
 					BEGIN
-						DECLARE @final_grade int,
-						@album_id int = (SELECT album_id FROM albums WHERE student_id = @student_id),
-						@academic_year int = (SELECT TOP 1 academic_year_id FROM academic_year ORDER BY academic_year_id DESC);
+						DECLARE @final_grade INT,
+						@album_id INT = (SELECT album_id FROM albums WHERE student_id = @student_id),
+						@academic_year INT = (SELECT TOP 1 academic_year_id FROM academic_year ORDER BY academic_year_id DESC);
 
 						SELECT @final_grade = ROUND(AVG(grade_name),0) FROM grades 
 							WHERE @student_id = student_id AND @subject_id = subject_id
@@ -119,15 +121,15 @@ BEGIN
 	DEALLOCATE kursor_studenci;
 	--koniec kursor_studenci
 
-	DECLARE @academic_year_id INT = (SELECT academic_year_id FROM inserted),
-			@academic_year_name DATETIME = (SELECT academic_year_name FROM inserted);
+	DECLARE @academic_year_id INT = (SELECT academic_year_id FROM INSERTED),
+			@academic_year_name DATETIME = (SELECT academic_year_name FROM INSERTED);
 	INSERT INTO academic_year VALUES(@academic_year_id, @academic_year_name);
 END
-go
+GO
 	INSERT INTO academic_year VALUES(2, '2018/10/15');
 
-	SELECT* from final_grades
-	SELECT* from academic_year
+	SELECT* FROM final_grades
+	SELECT* FROM academic_year
 
 
 
@@ -137,19 +139,19 @@ go
 
 
 	--Wyzwalacz DELETE_STUDENT, który przy próbie usuniêcia studenta w pierwszej kolejnoœci
---wypisuje go ze wszystkich przedmiotów na które jest zapisany, a nastêpnie trwale usuwa go z listy studentów.
- SELECT* from students WHERE student_id = 600
+--wypisuje GO ze wszystkich przedmiotów na które jest zapisany, a nastêpnie trwale usuwa GO z listy studentów.
+ SELECT* FROM students WHERE student_id = 600
  IF OBJECT_ID ('DELETE_STUDENT' , 'TR') IS NOT NULL
- DROP trigger DELETE_STUDENT ;
-go
+ DROP TRIGGER DELETE_STUDENT ;
+GO
 CREATE TRIGGER DELETE_STUDENT
 ON students
 INSTEAD OF DELETE
 AS
 BEGIN
 
-	DECLARE @student_id int = (SELECT student_id FROM deleted),
-			@ilosc int,
+	DECLARE @student_id INT = (SELECT student_id FROM deleted),
+			@ilosc INT,
 			@first_name VARCHAR(20) = (SELECT first_name FROM deleted),
 			@last_name VARCHAR(25) = (SELECT last_name FROM deleted)
 	
@@ -171,35 +173,35 @@ BEGIN
 	DELETE FROM students 
 	WHERE student_id = @student_id
 
-	print 'Studenta ' + @first_name + ' ' + @last_name + ' wypisano z ' + cast(@ilosc as VARCHAR) + ' przedmiotów, a nastepnie usuniêto z listy studentów.'
+	PRINT 'Studenta ' + @first_name + ' ' + @last_name + ' wypisano z ' + cast(@ilosc as VARCHAR) + ' przedmiotów, a nastepnie usuniêto z listy studentów.'
 
 END
-go
+GO
 	DELETE FROM students
 	WHERE student_id = 600;
 
-	SELECT* from students WHERE student_id = 600
+	SELECT* FROM students WHERE student_id = 600
 
 	--Wyzwalacz UPDATE_GRADE, który po ka¿dej modyfikacji w tabeli "grades"
 --wprowadza aktualn¹ dat¹ modyfikacji do pola "grade_date".
- SELECT* from grades WHERE grade_id = 5
+ SELECT* FROM grades WHERE grade_id = 5
  IF OBJECT_ID ('UPDATE_GRADE' , 'TR') IS NOT NULL
- DROP trigger UPDATE_GRADE ;
-go
+ DROP TRIGGER UPDATE_GRADE ;
+GO
 CREATE TRIGGER UPDATE_GRADE
 ON grades
 AFTER UPDATE
 AS
 BEGIN
-	DECLARE @grade_id int = (SELECT grade_id FROM inserted)
+	DECLARE @grade_id INT = (SELECT grade_id FROM INSERTED)
 
 	UPDATE grades
 	SET grade_date = GETDATE()
 	WHERE @grade_id = grade_id;
 END
-go
+GO
 	UPDATE grades
 	SET grade_name = 1
 	WHERE grade_id = 5;
 
-	SELECT* from grades WHERE grade_id = 5
+	SELECT* FROM grades WHERE grade_id = 5
